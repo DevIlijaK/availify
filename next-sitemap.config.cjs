75e5f094c1115508d90be07bf394c98f2447a4fc
Ilija Kosanin <ilija@evolvewell.com>
2024-12-22 14:07:17 +0100

    sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
-  siteUrl: "https://availify-tau.vercel.app/",
-  generateRobotsTxt: true,
+  siteUrl: "https://availify-tau.vercel.app",
+  generateRobotsTxt: true, // Optional: if you want to generate a robots.txt file
+  sitemapSize: 5000, // Optional: the number of URLs per sitemap
 };
